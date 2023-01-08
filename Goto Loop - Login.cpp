ba06d2151c8f848c03302b4544fc6d11eda89bd3
Ruzzel Dean <111668375+RuzzelDean@users.noreply.github.com>
2023-01-08 10:58:01 +0800

    Create Goto Loop - Login.cpp
    Login with goto (loop) and if-else condition.

---
+++ Goto Loop - Login.cpp
@@ -0,0 +1,38 @@
+#include <iostream.h>
+#include <conio.h>
+#include <string.h>
+
+main() {
+	char username[20], password[20], attempts=0;
+	
+	top:
+	clrscr();
+	
+	gotoxy(30, 12); cout<<"Username: ";
+	gotoxy(30, 13); cout<<"Password: ";
+	
+	gotoxy(40, 12); cin>>username;
+	gotoxy(40, 13); cin>>password;
+	
+	attempts++;
+	
+	if ((strcmp(username, "admin")==0 && strcmp(password, "xyz")==0)) {
+		gotoxy(30, 15); cout<<"Welcome Admin";
+		goto exit;
+	}
+
+	else if (attempts < 3) {
+		gotoxy(30, 15); cout<<"Invalid Credentials";
+		getch();
+		goto top;
+	}
+	
+	else {
+		gotoxy(30, 15); cout<<"Invalid Credentials";
+		gotoxy(30, 16); cout<<"Maximum Attempts reached!";
+	}
+	
+	exit:
+	getch();
+	return 0;
+}
